c743679bf2bc765b2afe69777bf6f0bbad782027
Sea-zone <jseazone7@gmail.com>
2023-08-23 13:29:57 +0545

    first

---
+++ newtab.js
@@ -0,0 +1,38 @@
+async function fetchQuote() {
+  const response = await fetch("https://api.quotable.io/random", {
+    method: "GET",
+  });
+  const data = await response.json();
+  return data;
+}
+
+// Display a quote on the new tab page
+async function displayQuote() {
+  const quoteElement = document.getElementById("quote");
+  const authorElement = document.getElementById("author");
+
+  const quoteData = await fetchQuote();
+  console.log(quoteData);
+  quoteElement.textContent = quoteData.content;
+  authorElement.textContent = "- " + quoteData.author;
+}
+
+// Copy quote to clipboard
+function copyToClipboard() {
+  const quoteElement = document.getElementById("quote");
+  const authorElement = document.getElementById("author");
+
+  const textToCopy = `"${quoteElement.textContent}" - ${authorElement.textContent}`;
+  const tempInput = document.createElement("textarea");
+  tempInput.value = textToCopy;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+}
+
+// Event listeners
+document?.addEventListener("DOMContentLoaded", displayQuote);
+document
+  .getElementById("copy-button")
+  ?.addEventListener("click", copyToClipboard);
